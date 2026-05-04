16b8ec637b2b0127a97f1b218d2f799bb1e0e1ef
Yara Nehme <ynehme2007@gmail.com>
2026-05-04 12:09:51 +0200

    exam: q10 - boids: screen wrapping

--- boids_exam.py
+++ boids_exam.py
@@ -59,7 +59,17 @@ class Boid:
     # Screen wrapping: if a boid goes off one edge of the screen, 
     # it should reappear on the opposite edge
     def _screen_wrap(self) -> None:
-        pass
+
+        if self.x < 0:
+            self.x = config.WIDTH
+        elif self.x > config.WIDTH:
+            self.x = 0
+
+
+        if self.y < 0:
+            self.y = config.HEIGHT
+        elif self.y > config.HEIGHT:
+            self.y = 0
     
     # Default wall behavior is bounce: if a boid hits the edge of the screen, 
     # it should bounce back in the opposite direction
